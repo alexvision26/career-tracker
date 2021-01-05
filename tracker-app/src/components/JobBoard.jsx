b11748c6838e5fdfcc183fe82ca2a40402022714
Alex <alexmillerprojects@gmail.com>
2021-01-04 16:09:49 -0800

    pausing DND

--- tracker-app/src/components/JobBoard.jsx
+++ tracker-app/src/components/JobBoard.jsx
@@ -12,6 +12,7 @@ import {
 } from "@material-ui/icons";
 import UpdateJobModal from "./UpdateJobModal";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuid } from "uuid";
 
 const useStyles = makeStyles((theme) => ({
   boardContainer: {
@@ -82,17 +83,6 @@ function JobBoard() {
   const [updateJobModal, setUpdateJobModal] = useState(false);
   const userId = localStorage.getItem("user");
   const getJobs = useSelector((state) => state.job_board);
-
-  const statusColumns = {
-    1: { name: "Interested", cards: getJobs },
-    2: { name: "Applied", cards: [] },
-    3: { name: "Reached out", cards: [] },
-    4: { name: "Interview", cards: [] },
-    5: { name: "Offer", cards: [] },
-    6: { name: "Not moving forward", cards: [] },
-  };
-
-  const [columns, setColumns] = useState(statusColumns);
   const [currEdit, setCurrEdit] = useState("");
 
   const handleUpdateOpen = (id) => {
@@ -137,95 +127,60 @@ function JobBoard() {
     reloadJobBoard();
   }, []);
 
-  // const jobSorter = (jobs, cat) => {
-  //   const renderIcon = () => {
-  //     switch (cat) {
-  //       case "Interested":
-  //         return <BubbleChartIcon className={classes.cardIcon} />;
-  //       case "Applied":
-  //         return <AssignmentIndIcon className={classes.cardIcon} />;
-  //       case "Reached out":
-  //         return <SettingsInputAntennaIcon className={classes.cardIcon} />;
-  //       case "Interview":
-  //         return <SupervisedUserCircleIcon className={classes.cardIcon} />;
-  //       case "Offer":
-  //         return <AssignmentTurnedInIcon className={classes.cardIcon} />;
-  //       case "Not moving forward":
-  //         return <CancelIcon className={classes.cardIcon} />;
-  //     }
-  //   };
-  //   return jobs
-  //     .filter((j) => {
-  //       return j.status === cat;
-  //     })
-  //     .map((job, i) => {
-  //       return (
-  //         <>
-  //           <Card
-  //             key={job.jobTitle}
-  //             className={classes.cardContainer}
-  //             onClick={() => {
-  //               handleUpdateOpen(job._id);
-  //             }}
-  //             style={{
-  //               backgroundColor: `rgba(${job.color.r}, ${job.color.g}, ${job.color.b}, ${job.color.a} )`,
-  //             }}
-  //           >
-  //             <div className={classes.cardContent}>
-  //               {renderIcon()}
-  //               <div className={classes.cardTitleCont}>
-  //                 <h3 style={{ margin: 0 }} className={classes.cardTitle}>
-  //                   {job.jobTitle}
-  //                 </h3>
-  //                 <p style={{ margin: 0, fontSize: ".9rem" }}>{job.company}</p>
-  //               </div>
-  //             </div>
-  //             {formatDate(job.created, cat)}
-  //           </Card>
-  //         </>
-  //       );
-  //     });
-  // };
-
-  const onDragEnd = (result, columns, setColumns) => {
-    if (!result.destination) return;
-    const { source, destination } = result;
-
-    if (source.droppableId !== destination.droppableId) {
-      const sourceColumn = columns[source.droppableId];
-      const destColumn = columns[destination.droppableId];
-      const sourceItems = [...sourceColumn.items];
-      const destItems = [...destColumn.items];
-      const [removed] = sourceItems.splice(source.index, 1);
-      destItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...sourceColumn,
-          items: sourceItems,
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: destItems,
-        },
+  const jobSorter = (jobs, cat) => {
+    const renderIcon = () => {
+      switch (cat) {
+        case "Interested":
+          return <BubbleChartIcon className={classes.cardIcon} />;
+        case "Applied":
+          return <AssignmentIndIcon className={classes.cardIcon} />;
+        case "Reached out":
+          return <SettingsInputAntennaIcon className={classes.cardIcon} />;
+        case "Interview":
+          return <SupervisedUserCircleIcon className={classes.cardIcon} />;
+        case "Offer":
+          return <AssignmentTurnedInIcon className={classes.cardIcon} />;
+        case "Not moving forward":
+          return <CancelIcon className={classes.cardIcon} />;
+      }
+    };
+    return jobs
+      .filter((j) => {
+        return j.status === cat;
+      })
+      .map((job, i) => {
+        return (
+          <>
+            <Card
+              key={job.jobTitle}
+              className={classes.cardContainer}
+              onClick={() => {
+                handleUpdateOpen(job._id);
+              }}
+              style={{
+                backgroundColor: `rgba(${job.color.r}, ${job.color.g}, ${job.color.b}, ${job.color.a} )`,
+              }}
+            >
+              <div className={classes.cardContent}>
+                {renderIcon()}
+                <div className={classes.cardTitleCont}>
+                  <h3 style={{ margin: 0 }} className={classes.cardTitle}>
+                    {job.jobTitle}
+                  </h3>
+                  <p style={{ margin: 0, fontSize: ".9rem" }}>{job.company}</p>
+                </div>
+              </div>
+              {formatDate(job.created, cat)}
+            </Card>
+          </>
+        );
       });
-    } else {
-      const column = columns[source.droppableId];
-      const copiedItems = [...column.items];
-      const [removed] = copiedItems.splice(source.index, 1);
-      copiedItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...column,
-          items: copiedItems,
-        },
-      });
-    }
   };
 
   // <h3 className={classes.title}>{col}</h3>
   // {jobSorter(getJobs, col)}
+
+  // const [columns, setColumns] = useState(statusColumns);
 
   return (
     <>
@@ -237,67 +192,36 @@ function JobBoard() {
       />
 
       <Container maxWidth>
-        <DragDropContext
-          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        <Grid
+          container
+          justify="space-evenly"
+          className={classes.boardContainer}
         >
-          <Grid
-            container
-            justify="space-evenly"
-            className={classes.boardContainer}
-          >
-            {Object.entries(columns).map(([colId, col], index) => {
-              return (
-                <div key={colId}>
-                  <h2>{col.name}</h2>
-                  <Droppable droppableId={colId} key={colId}>
-                    {(provided, snapshot) => {
-                      return (
-                        <div
-                          {...provided.droppableProps}
-                          ref={provided.innerRef}
-                        >
-                          {col.cards.map((item, index) => {
-                            return (
-                              <Draggable
-                                key={item._id}
-                                draggableId={item._id}
-                                index={index}
-                              >
-                                {(provided, snapshot) => {
-                                  return (
-                                    <div
-                                      ref={provided.innerRef}
-                                      {...provided.draggableProps}
-                                      {...provided.dragHandleProps}
-                                      style={{
-                                        userSelect: "none",
-                                        padding: 16,
-                                        margin: "0 0 8px 0",
-                                        minHeight: "50px",
-                                        backgroundColor: snapshot.isDragging
-                                          ? "#263B4A"
-                                          : "#456C86",
-                                        color: "white",
-                                        ...provided.draggableProps.style,
-                                      }}
-                                    >
-                                      {item.jobTitle}
-                                    </div>
-                                  );
-                                }}
-                              </Draggable>
-                            );
-                          })}
-                          {provided.placeholder}
-                        </div>
-                      );
-                    }}
-                  </Droppable>
-                </div>
-              );
-            })}
+          <Grid item xs={2} className={classes.column}>
+            <h3 className={classes.title}>Interested</h3>
+            {jobSorter(getJobs, "Interested")}
           </Grid>
-        </DragDropContext>
+          <Grid item xs={2}>
+            <h3 className={classes.title}>Applied</h3>
+            {jobSorter(getJobs, "Applied")}
+          </Grid>
+          <Grid item xs={2}>
+            <h3 className={classes.title}>Reached out</h3>
+            {jobSorter(getJobs, "Reached out")}
+          </Grid>
+          <Grid item xs={2}>
+            <h3 className={classes.title}>Interview</h3>
+            {jobSorter(getJobs, "Interview")}
+          </Grid>
+          <Grid item xs={2}>
+            <h3 className={classes.title}>Offer</h3>
+            {jobSorter(getJobs, "Offer")}
+          </Grid>
+          <Grid item xs={2}>
+            <h3 className={classes.title}>Not moving forward</h3>
+            {jobSorter(getJobs, "Not moving forward")}
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
